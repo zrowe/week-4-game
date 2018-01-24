71175e778dcdd7557c896e6f0da51d4fa1f8c363
zrowe1 <paul@boundary.com>
2018-01-24 00:53:18 -0800

    adding wireframe for play board

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+/*
+
+	each char is an object wth properties.
+
+	name:
+	image: 
+	status:   idle, player, defender, dead
+	healthPoints:
+	currentattackPower:
+	baseAttackPower:
+
+
+attackButton: causes attacker to attack followed by defender's counter attack 
+
+
+
+*/
+// Blueprint for a character
+function Character(name, 
+	status, 
+	picture, 
+	healthPoints, 
+	baseAttackPower, 
+	counterAttackPower)
+
+
+
+
+
